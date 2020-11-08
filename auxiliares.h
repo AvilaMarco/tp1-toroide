ae6f23c3dae2050dd600de7b778ac4dd3e8254e0
david444456 <david2toranzo2@gmail.com>
2020-11-08 14:34:29 -0300

    1,2,3,4,7,9

--- auxiliares.h
+++ auxiliares.h
@@ -10,5 +10,12 @@ using namespace std;
 // definir aqui las funciones
 pair<int,int> mp(int a, int b);
 vector<posicion> ordenar(vector<posicion> &v);
+bool esToroide(vector<vector<bool>> t);
+bool enRangoToroide(int f, int c, vector<vector<bool>> t);
+int cantidadVivas(toroide t);
+int superficieTotal(toroide t);
 
+int vecinosVivos(toroide t, int j, int c);
+
+bool debeVivir(toroide t, int j, int c);
 #endif //REUNIONESREMOTAS_AUXILIARES_H
